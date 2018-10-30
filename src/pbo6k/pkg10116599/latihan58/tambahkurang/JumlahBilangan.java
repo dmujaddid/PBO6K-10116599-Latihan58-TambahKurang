71ac9ec16add88a7660bec294543247942f41dc0
Roby <robyeka@email.unikom.ac.id>
2018-10-31 04:14:45 +0700

    ngoding

---
+++ src/pbo6k/pkg10116599/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan58.tambahkurang;
+
+/**
+ *
+ * @author eka
+ */
+public class JumlahBilangan extends Bilangan {
+
+    
+    public void tampilHasilJumlah() {
+        
+    }
+    
+   
+}
